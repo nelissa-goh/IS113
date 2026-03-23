78546349922b8ec7a4940d3c8c2f01d4f41be2de
joyhardi <134623598+joyhardi@users.noreply.github.com>
2026-03-23 08:16:53 +0800

    Add files via upload

---
+++ routes/list-routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const listController = require('../controllers/list-controller');
+
+const router = express.Router(); // sub application
+//create new watchlist
+router.post('/users/:userId/watchlist/create', listController.createListInfo);
+
+//display watch list
+router.get('/users/:userId/watchlist', listController.displayList);
+
+//add movies
+router.get('/users/:userId/add-movies', listController.showAddForm);
+router.post('/users/:userId/add-movies', listController.addOrUpdateList);
+
+//update watched status
+router.get("/users/:userId/update-status", listController.showUpdateForm);
+router.post('/users/:userId/update-status', listController.markWatched);
+
+//delete movies
+router.get('/users/:userId/delete-movies', listController.showDeleteForm);
+router.post('/users/:userId/delete-movies', listController.removeFromList);
+
+// Delete entire list (optional)
+// router.post('/users/:userId/delete-list', listController.deleteList);
+
+// EXPORT
+module.exports = router;
