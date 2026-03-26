2f3168e4fc9badee1b524d25fb5c4fde8d37cc81
trixie-tan <trixie.tan.2025@computing.smu.edu.sg>
2026-03-26 10:11:09 +0800

    Add files via upload

---
+++ models/favourites-model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+// Create a new ‘favourites' schema
+const favouriteSchema = new mongoose.Schema({
+    user : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    movieList: [{
+        movie: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Movie',
+            required: true
+        },
+        isFavourite: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
+    }]
+});
+
+const Favourite = mongoose.model('Favourite', favouriteSchema,'favourite');
+
+//Methods here
+
+exports.Favourite = Favourite;
+
+exports.retrieveAll = function() {
+  return Favourite.find().populate('movieList.movie');
+};
+
+exports.addFavourites = function(newFavourite){
+    return Favourite.create(newFavourite);
+};
+
+exports.deleteFavourites = function(favouriteId){
+    return Favourite.deleteOne({ _id: favouriteId});
+}
+
+exports.updateFavouriteStatus = function(userId, movieId, isFavourite) {
+  return Favourite.updateOne(
+    { user: userId, "movieList.movie": movieId },
+    { $set: { "movieList.$.isFavourite": isFavourite } }
+  );
+};
+
